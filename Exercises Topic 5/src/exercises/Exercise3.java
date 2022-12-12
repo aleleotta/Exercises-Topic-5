257c9f7ddac0bd311417db9ed763e0f46c51ba0a
aleleotta <aleleotta@106-02.info2>
2022-12-12 10:21:10 +0100

    .

--- Exercises Topic 5/src/exercises/Exercise3.java
+++ Exercises Topic 5/src/exercises/Exercise3.java
@@ -3,13 +3,21 @@ import java.util.*;
 
 public class Exercise3 {
 	public static void main(String[] args) {
+		double arrayStorage[] = new double[5];
+		double input;
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce a decimal number: ");
+		for(int i = 0; i > arrayStorage ; i++) {
+			arrayStorage[i];
+		}
+		for(int arrayEachInput: arrayStorage[i]) {}
+		System.out.println();
 		sc.close();
 	}
 }
-/*Diseñar un programa que solicite al usuario 5 números decimales y 
- *los almacene en una tabla. 
- *Utiliza el mismo bucle para solicitar los 5 números as
+/*Diseñar un programa que solicite al usuario
+ *5 números decimales y los almacene en una tabla.
+ *Utiliza el mismo bucle para solicitar los 5 números así
  *como para almacenarlos en la tabla.
  *A continuación, mostrar por consola los números
  *en el mismo orden en el que se han introducido.
