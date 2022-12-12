809fd60120c3cd3ee5b73670ed8d3eaa4d9a40d7
aleleotta <aleleotta@106-02.info2>
2022-12-12 09:44:13 +0100

    .

--- Exercises Topic 5/src/exercises/Exercise2.java
+++ Exercises Topic 5/src/exercises/Exercise2.java
@@ -2,13 +2,17 @@ package exercises;
 
 public class Exercise2 {
 	public static void main(String[] args) {
+		int arrayResult = 0;
 		int randomLength[] = new int[10]; //Array with length declared but no values have been assigned.
 		System.out.println("Printing...\n");
 		for(int i = 0; i < randomLength.length; i++) { //Always use a for cycle to print an array.
 			randomLength[i] = (int)(Math.random()* 100 + 1); //Math.random used to assign random values to the array from 1 to 100.
 			System.out.println(randomLength[i]); //Array printed inside for cycle.
-			
 		}
+		for(int value: randomLength) {
+			arrayResult = arrayResult + value;
+		}
+		System.out.println("Result: " + arrayResult);
 	}
 }
 /*Partiendo de la tabla creada en el ejercicio 1,
---
+++ Exercises Topic 5/src/exercises/Exercise3.java
@@ -0,0 +1,16 @@
+package exercises;
+import java.util.*;
+
+public class Exercise3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sc.close();
+	}
+}
+/*Diseñar un programa que solicite al usuario 5 números decimales y 
+ *los almacene en una tabla. 
+ *Utiliza el mismo bucle para solicitar los 5 números as
+ *como para almacenarlos en la tabla.
+ *A continuación, mostrar por consola los números
+ *en el mismo orden en el que se han introducido.
+ */
