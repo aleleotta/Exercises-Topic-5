b62e7dde751fc588989fd00cccd9b01693c252b1
aleleotta <aleleotta@106-02.info2>
2022-12-14 14:17:28 +0100

    Exercise 4 completed!

--- Exercises Topic 5/src/exercises/Exercise4.java
+++ Exercises Topic 5/src/exercises/Exercise4.java
@@ -1,10 +1,17 @@
 package exercises;
-import java.util.*;
 
 public class Exercise4 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		sc.close();
+		int num[] = new int[12];
+		num[0] = 39;
+		num[1] = -2;
+		num[4] = 0;
+		num[6] = 14;
+		num[8] = 5;
+		num[9] = 120;
+		for(int print: num) {
+			System.out.print(print + " ");
+		}
 	}
 }
 /*Define un array de 12 números enteros con nombre num y asigna
