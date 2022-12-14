fb10499839a7b1e0e7007d8833d192954878990d
aleleotta <aleleotta@106-02.info2>
2022-12-14 14:19:14 +0100

    .

--- Exercises Topic 5/src/exercises/Exercise5.java
+++ Exercises Topic 5/src/exercises/Exercise5.java
@@ -3,11 +3,17 @@ import java.util.*;
 
 public class Exercise5 {
 	public static void main(String[] args) {
+		double arrayStorage[] = new double[10];
 		Scanner sc = new Scanner(System.in);
+		System.out.print("Introduce a decimal number: ");
+		for(int i = 0; i < arrayStorage.length; i++) {
+			arrayStorage[i]= sc.nextDouble();
+		}
 		sc.close();
 	}
 }
 /*Crea un programa que pida diez números reales por teclado,
  *los almacene en un array,
- *y luego lo recorra para averiguar el máximo y mínimo y mostrarlos por pantalla.
+ *y luego lo recorra para averiguar
+ *el máximo y mínimo y mostrarlos por pantalla.
  */
