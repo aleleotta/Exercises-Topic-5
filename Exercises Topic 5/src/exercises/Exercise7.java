a10c465cc7a474c7a6ac92db905adcf6b143e095
aleleotta <aleleotta@106-02.info2>
2023-01-10 12:36:30 +0100

    .

--- Exercises Topic 5/src/exercises/Exercise7.java
+++ Exercises Topic 5/src/exercises/Exercise7.java
@@ -3,8 +3,18 @@ import java.util.*;
 
 public class Exercise7 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		sc.close();
+		int table[] = new int[10]; //Declaration of array with specific length.
+		Scanner sc = new Scanner(System.in); //Scanner declared.
+		System.out.print("Introduce a number: "); //Program asks for input.
+		for(int i = 0; i < table.length ; i++) { //Normal for cycle modifies.
+			table[i] = sc.nextInt(); //Modification of values within array on every single position.
+		}
+		System.out.print("[");
+		for(int tableQuery: table) { //For each cycle does queries.
+			System.out.print(tableQuery + " "); //Prints every single value within the array.
+		}
+		System.out.print("]");
+		sc.close(); //Scanner closed.
 	}
 }
 /*Escribe un programa que lea 10 números por teclado
