3a9854dee810b4f24ca4f8548ad26e4368d652b6
aleleotta <aleleotta@106-02.info2>
2023-01-11 08:54:07 +0100

    .

--- Exercises Topic 5/src/exercises/Exercise7.java
+++ Exercises Topic 5/src/exercises/Exercise7.java
@@ -4,13 +4,29 @@ import java.util.*;
 public class Exercise7 {
 	public static void main(String[] args) {
 		int table[] = new int[10]; //Declaration of array with specific length.
+		int table1[] = new int[10]; //Declaration of array with specific length.
 		Scanner sc = new Scanner(System.in); //Scanner declared.
-		System.out.print("Introduce a number: "); //Program asks for input.
 		for(int i = 0; i < table.length ; i++) { //Normal for cycle modifies.
+			System.out.print("Introduce a number: "); //Program asks for input.
 			table[i] = sc.nextInt(); //Modification of values within array on every single position.
 		}
 		System.out.print("[");
 		for(int tableQuery: table) { //For each cycle does queries.
+			System.out.print(tableQuery + " "); //Prints every single value within the array.
+		}
+		System.out.println("]");
+		table1[0] = table[9];
+		table1[1] = table[8];
+		table1[2] = table[7];
+		table1[3] = table[6];
+		table1[4] = table[5];
+		table1[5] = table[4];
+		table1[6] = table[3];
+		table1[7] = table[2];
+		table1[8] = table[1];
+		table1[9] = table[0];
+		System.out.print("[");
+		for(int tableQuery: table1) { //For each cycle does queries.
 			System.out.print(tableQuery + " "); //Prints every single value within the array.
 		}
 		System.out.print("]");
